d77723022f243f3ca608bd1631af2b343e2c1cae
Rocsan23 <rocsan23@gmail.com>
2023-10-18 11:44:50 +0200

    Problema 1

--- Trabajo.R
+++ Trabajo.R
@@ -15,7 +15,7 @@ sol_w<-criterio.Wald(m1)
 sol_w
 #Las mejores soluciones son d1 y d3 según el método wald
 
-#meto optimista
+#Metodo optimista
 sol_op<-criterio.Optimista(m1)
 sol_op
 
@@ -44,5 +44,45 @@ sol_pi<-criterio.PuntoIdeal(m1)
 sol_pi
 #La mejor oslucion segun el criterio de Punto Ideal es d3
 
-#Tabla desvarobale 
+#Tabla desfavorale  
+m2<-crea.tablaX( c(4,11,-2,0,5,-4,4,4,-3), 
+                 numalternativas = 3,
+                 numestados = 3)
+m2
 
+#Criterio Wald 
+
+sol_des_wa<-criterio.Wald(m2,favorable = T)
+sol_des_wa
+
+#La mejor solucion segun el metodo wald es d1 
+
+#Criterio optimista 
+sol_des_op<-criterio.Optimista(m2,favorable = T)
+sol_des_op
+#La menjor solucion segun el criterio optimista es d1 
+
+
+#Criterio Hurwicz 
+sol_des_Hu<-criterio.Hurwicz(m2,favorable = T)
+sol_des_Hu
+
+#La mejor solucion segun el criterio Hurwicz es d1 
+
+#Criterio Savage 
+sol_des_sav<-criterio.Savage(m2, favorable = T)
+sol_des_sav
+#La mejor solcuion segun el criterio savage es d1 
+
+#Criterio Laplace 
+sol_des_La<-criterio.Laplace(m2, favorable = T)
+sol_sa
+#La mejor solcuion segun el creiterio Laplace es d1 
+
+
+
+#Criterio Punto Ideal 
+sol_des_PI<-criterio.PuntoIdeal(m2, favorable = T)
+sol_des_PI
+
+#La mejor solucion segun el criterio de punto ideal es d1 
